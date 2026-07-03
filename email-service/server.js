7fdc0e273b7d9e3f299915ae580b52afc8ed92c8
NandiniSannareddy <nandinisannareddy@gmail.com>
2026-07-03 13:48:28 +0530

    added port in email service

--- email-service/server.js
+++ email-service/server.js
@@ -2,13 +2,24 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import { connectConsumer } from './config/kafka.js';
 import { startConsumer } from './emailConsumer.js';
+import express from 'express';
 
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+    res.send("Email Service is running");
+});
 
 const startserver=async()=>{
     try{
         await connectDB();
         await connectConsumer();
+        app.listen(PORT, () => {
+            console.log(`Email Service health server running on port ${PORT}`);
+        })
         await startConsumer();
     }
     catch(error){
